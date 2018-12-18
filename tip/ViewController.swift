f0deedc604d16149100806e6eeeda2bd5e41ad22
Haiming WU <haiminw@uci.edu>
2018-12-18 15:19:04 -0800

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Haiming Wu on 12/18/18.
+//  Copyright © 2018 Haiming Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
