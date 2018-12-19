47d1e4ddef9c1dce05ecadc928f71bfc648d2930
Haiming WU <haiminw@uci.edu>
2018-12-18 19:31:23 -0800

    added .gitattributes and .gitignore

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func whentap(_ sender: Any) {
+        view.endEditing(true)
+        }
+ 
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentage = [0.08, 0.13, 0.23]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    
+    
 }
 
